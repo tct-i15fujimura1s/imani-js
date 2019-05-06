9688bd72e152140a5dcd3fad6c191a20d4b43056
tct-i15fujimura1s <44060475+tct-i15fujimura1s@users.noreply.github.com>
2019-05-06 21:58:26 +0900

    Update imani.js

--- imani.js
+++ imani.js
@@ -1,4 +1,16 @@
 class IMani {
+  static concatH(...imgs) {
+    const im = new IMani();
+    imgs.forEach(i => im.put(i, im.width));
+    return im;
+  }
+  
+  static concatV(...imgs) {
+    const im = new IMani();
+    imgs.forEach(i => im.put(i, 0, im.height));
+    return im;
+  }
+  
   constructor(width = 0, height = 0) {
     const c = this.canvas = document.createElement("canvas");
     [c.width, c.height] = [width, height];
@@ -23,7 +35,7 @@ class IMani {
   
   get image() {
     const i = new Image();
-    i.src = URL.createObjectURL(this.canvas.toBlob("image/png"));
+    this.canvas.toBlob(blob => i.src = URL.createObjectURL(blob));
     return i;
   }
   
@@ -36,6 +48,7 @@ class IMani {
   }
   
   setSize(width = null, height = null) {
+    if(this.canvas.width == 0 || this.canvas.height == 0) this.canvas.width = this.canvas.height = 1;
     const data = this.imageData;
     if(typeof width  === "number") this.canvas.width  = width;
     if(typeof height === "number") this.canvas.height = height;
@@ -57,6 +70,9 @@ class IMani {
       this.context.putImageData(i, x, y);
     } else if(i instanceof IMani) {
       return this.put(i.canvas, x, y);
+    } else {
+      //TODO: string
+      throw new TypeError();
     }
     return this;
   }
