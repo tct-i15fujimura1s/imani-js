60510865d9669357d3694d242be0ec07c06abd4a
tct-i15fujimura1s <44060475+tct-i15fujimura1s@users.noreply.github.com>
2019-05-07 06:00:21 +0900

    Update imani.js

--- imani.js
+++ imani.js
@@ -11,10 +11,11 @@ class IMani {
     return im;
   }
   
-  constructor(width = 0, height = 0) {
+  constructor(img = null, width = 0, height = 0) {
     const c = this.canvas = document.createElement("canvas");
     [c.width, c.height] = [width, height];
     this.context = c.getContext("2d");
+    if(img != null) put(img);
   }
   
   get width() {
@@ -40,6 +41,7 @@ class IMani {
   }
   
   get imageData() {
+    if(this.canvas.width == 0 || this.canvas.height == 0) this.canvas.width = this.canvas.height = 1;
     return this.context.getImageData(0, 0, this.width, this.height);
   }
   
@@ -47,8 +49,19 @@ class IMani {
     this.context.putImageData(data, 0, 0);
   }
   
+  get clone() {
+    return new IMani(this, this.canvas.width, this.canvas.height);
+  }
+  
+  get cloneCanvas() {
+    const cc = document.createElement("canvas");
+    [cc.width, cc.height] = [this.canvas.width, this.canvas.height];
+    const cctx = cc.getContext("2d");
+    cctx.drawImage(this.canvas, 0, 0);
+    return cc;
+  }
+  
   setSize(width = null, height = null) {
-    if(this.canvas.width == 0 || this.canvas.height == 0) this.canvas.width = this.canvas.height = 1;
     const data = this.imageData;
     if(typeof width  === "number") this.canvas.width  = width;
     if(typeof height === "number") this.canvas.height = height;
@@ -70,10 +83,39 @@ class IMani {
       this.context.putImageData(i, x, y);
     } else if(i instanceof IMani) {
       return this.put(i.canvas, x, y);
+    } else if(typeof i === "string") {
+      const w = this.context.measureText(i);
+      this.extend(w, 0);
+      this.context.fillText(i, x, y);
     } else {
       //TODO: string
       throw new TypeError();
     }
+    return this;
+  }
+  
+  clear() {
+    this.context.clearRect(0, 0, this.width, this.height);
+    return this;
+  }
+  
+  translate(x, y) {
+    const cc = this.cloneCanvas;
+    this.extend(x + this.width, y + this.height).clear();
+    this.context.translate(x, y);
+    this.context.drawImage(cc, 0, 0);
+    this.context.translate(0, 0);
+    return this;
+  }
+  
+  rotate(deg = 0, cx = this.width * 0.5, cy = this.height * 0.5) {
+    const cc = this.cloneCanvas;
+    this.clear();
+    this.context.translate(cx, cy);
+    this.context.rotate(deg / 180 * Math.PI);
+    this.context.drawImage(cc, -cc.width * 0.5, -cc.height * 0.5);
+    this.context.rotate(0);
+    this.context.translate(0, 0);
     return this;
   }
 }
